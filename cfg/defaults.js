4e6c25a5269b5a643c9cf91eb4d07ca9cbeb7023
jingpei <jingjpei@gmail.com>
2016-03-22 10:04:08 -0700

    App scaffold

---
+++ cfg/defaults.js
@@ -0,0 +1,47 @@
+'use strict';
+var autoprefixer = require('autoprefixer')
+var precss = require('precss')
+
+const path = require('path');
+const srcPath = path.join(__dirname, '/../src');
+const dfltPort = 8000;
+function getDefaultModules() {
+  return {
+    preLoaders: [{
+        test: /\.(js|jsx)$/,
+        include: srcPath,
+        loader: 'eslint-loader'
+      }],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      },
+      {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!postcss-loader!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!postcss-loader!stylus-loader'
+      }
+    ]
+  };
+}
+module.exports = {
+  srcPath: srcPath,
+  publicPath: '/assets/',
+  port: dfltPort,
+  getDefaultModules: getDefaultModules,
+  postcss: function () {
+    return [autoprefixer, precss];
+  }
+};
